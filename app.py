2b6927ff08405efa23f0a3182073d2d40eecf5e5
nomunb <prontobronto@protonmail.com>
2021-08-13 20:04:45 +0800

    * initialized flask app * added hello world endpoint

---
+++ app.py
@@ -0,0 +1,29 @@
+import flask
+import flask_sqlalchemy
+import flask_jwt_extended
+
+db = flask_sqlalchemy.SQLAlchemy()
+jwt = flask_jwt_extended.JWTManager()
+
+def create_app():
+    app = flask.Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipe.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = "dobcehc30dhc3db4c0u34cb0agwrg"
+    app.config['JWT_TOKEN_LOCATION'] = 'cookies'
+    db.init_app(app)
+    jwt.init_app(app)
+
+
+    @app.route("/test") #initialized flask app
+    def helloworld(): #added helloworld endpoint
+        return "helloworld"
+
+    return app
+
+
+
+#name
+if __name__ == "__main__":
+    create_app().run(debug=True)
+
